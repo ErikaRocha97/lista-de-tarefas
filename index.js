189757b6815c749a4499e3ec983b19087ec9c1b8
Erika Rocha <erikaarosio@hotmail.com>
2025-03-05 18:02:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,102 @@ const todoListUl = document.querySelector("#todo-list");
 // [{title: Tarefa 1, done: true} , ...]
 let tasks = [];
 
+// ! Adicionar tarefa no HTML
+function renderTaskOnHTML(taskTitle, done = false) {
+  // criar tag li no javascrit: <li></li>
+  const li = document.createElement("li");
+  // li.textContent = taskTitle;
+
+  // criar um input: <input type="checkbox" />
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  input.addEventListener("change", (event) => {
+    // pegar o elemento pai
+    const liToToggle = event.target.parentElement;
+
+    const spanToToggle = liToToggle.querySelector("span");
+
+    const done = event.target.checked;
+    // console.log(event.target.checked);
+    if (done) {
+      spanToToggle.style.textDecoration = "line-through";
+    } else {
+      spanToToggle.style.textDecoration = "none";
+    }
+
+    // console.log(tasks);
+
+    // percorrer todas as tarefas. Alterar o done.
+    tasks = tasks.map((t) => {
+      if (t.title === spanToToggle.textContent) {
+        return {
+          title: t.title,
+          done: !t.done,
+        };
+      }
+
+      return t;
+    });
+
+    // console.log(tasks);
+    // ! Salvar no localstorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  input.checked = done;
+
+  const span = document.createElement("span");
+  span.textContent = taskTitle;
+
+  if (done) {
+    span.style.textDecoration = "line-through";
+  }
+
+  const button = document.createElement("button");
+  button.textContent = "Remover";
+
+  // Adicionar evento "escutador" no botão
+  button.addEventListener("click", (event) => {
+    const liToRemove = event.target.parentElement;
+
+    // Pegar o título da tarefa que estou removendo para remover do Array.
+    const titleToRemove = liToRemove.querySelector("span").textContent;
+
+    // Percorrer cada tarefa. Se o título for diferente do que eu quero remover, manter no Array final. Se não, excluir.
+    tasks = tasks.filter((t) => t.title !== titleToRemove);
+
+    // Remover do HTML o "elemento pai" do botão que disparou o evento:
+    // console.log(event.target.parentElement);
+    todoListUl.removeChild(liToRemove);
+
+    // console.log(tasks);
+
+    // ! Salvar no localstorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  // Inserir no HTML
+
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  todoListUl.appendChild(li);
+}
+
+// Pegar tarefas do localstorage ao carregar a página
+window.onload = () => {
+  const tasksOnLocalStorage = localStorage.getItem("tasks");
+  if (!tasksOnLocalStorage) return;
+
+  // converte String em objeto. Conteúdo do locastorage colocado no array.
+  tasks = JSON.parse(tasksOnLocalStorage);
+
+  tasks.forEach((t) => {
+    renderTaskOnHTML(t.title, t.done);
+  });
+};
+
 // Lidar com o evento de submissão do formulário
 // Adicionar tarefa
 
@@ -30,43 +126,11 @@ form.addEventListener("submit", (event) => {
     done: false,
   });
 
-  // criar tag li no javascrit: <li></li>
-  const li = document.createElement("li");
-  // li.textContent = taskTitle;
+  // ! Salvar no localstorage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
-  // criar um input: <input type="checkbox" />
-  const input = document.createElement("input");
-  input.setAttribute("type", "checkbox");
-
-  const span = document.createElement("span");
-  span.textContent = taskTitle;
-
-  const button = document.createElement("button");
-  button.textContent = "Remover";
-
-  // Adicionar evento "escutador" no botão
-  button.addEventListener("click", (event) => {
-    const liToRemove = event.target.parentElement;
-
-    // Pegar o título da tarefa que estou removendo para remover do Array.
-    const titleToRemove = liToRemove.querySelector("span").textContent;
-
-    // Percorrer cada tarefa. Se o título for diferente do que eu quero remover, manter no Array final. Se não, excluir.
-    tasks.filter((t) => t.title !== titleToRemove);
-
-    // Remover do HTML o "elemento pai" do botão que disparou o evento:
-    // console.log(event.target.parentElement);
-    todoListUl.removeChild(liToRemove);
-    // Remover do array de tarefas:
-  });
-
-  // Inserir no HTML
-
-  li.appendChild(input);
-  li.appendChild(span);
-  li.appendChild(button);
-
-  todoListUl.appendChild(li);
+  // Adicionando a nova tarefa no HTML chamando a função:
+  renderTaskOnHTML(taskTitle);
 
   taskTitleInput.value = "";
 });
